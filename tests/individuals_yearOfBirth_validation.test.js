0dbb54fb3840812fc18489b7681cdc300013473a
dcruvolo <davidruvolo51@gmail.com>
2025-08-22 10:30:33 +0200

    feat: added tests; updated linting

--- tests/individuals_yearOfBirth_validation.test.js
+++ tests/individuals_yearOfBirth_validation.test.js
@@ -1,22 +1,19 @@
-import { expect, test, describe } from 'vitest';
-import validateYearOfBirth from '../model/expressions/individuals_yearOfBirth_validation';
+import { expect, test, describe } from "vitest";
+import validateYearOfBirth from "../src/js/individuals_yearOfBirth_validation";
 
-describe(
-  "year of birth",
-  () => {
-    const currentYear = new Date().getUTCFullYear();
-    const nextYear = currentYear + 1;
-    
-    test("year must be less than the current year", () => {
-      expect(validateYearOfBirth(nextYear)).toContain("Year of birth");
-    });
-    
-    test("year must be more than 1900", () => {
-      expect(validateYearOfBirth(1800)).toContain("Year of birth");
-    })
-    
-    test("year of birth is valid", () => {
-      expect(validateYearOfBirth(currentYear - 1)).toBeTruthy();
-    })
-  }
-)
+describe("year of birth", () => {
+  const currentYear = new Date().getUTCFullYear();
+  const nextYear = currentYear + 1;
+
+  test("year must be less than the current year", () => {
+    expect(validateYearOfBirth(nextYear)).toContain("Year of birth");
+  });
+
+  test("year must be more than 1900", () => {
+    expect(validateYearOfBirth(1800)).toContain("Year of birth");
+  });
+
+  test("year of birth is valid", () => {
+    expect(validateYearOfBirth(currentYear - 1)).toBeTruthy();
+  });
+});
