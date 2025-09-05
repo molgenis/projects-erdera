1e2e111697db54661d840f8b4b7cee2557310235
dcruvolo <davidruvolo51@gmail.com>
2025-09-05 09:59:04 +0200

    chore: added unit test

--- tests/individuals_yearOfBirth_validation.test.js
+++ tests/individuals_yearOfBirth_validation.test.js
@@ -5,15 +5,16 @@ describe("year of birth", () => {
   const currentYear = new Date().getUTCFullYear();
   const nextYear = currentYear + 1;
 
-  test("year must be less than the current year", () => {
+  test("year cannot be later than the current year", () => {
     expect(validateYearOfBirth(nextYear)).toContain("Year of birth");
   });
 
-  test("year must be more than 1900", () => {
+  test("year cannot be earlier than 1900", () => {
     expect(validateYearOfBirth(1800)).toContain("Year of birth");
+    expect(validateYearOfBirth(180)).toContain("Year of birth");
   });
-
-  test("year of birth is valid", () => {
+  
+  test("year is valid", () => {
     expect(validateYearOfBirth(currentYear - 1)).toBeTruthy();
   });
 });
