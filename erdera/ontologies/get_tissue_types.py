d0af8a41780454ccbd64388c8122af5dc537bc1e
dcruvolo <davidruvolo51@gmail.com>
2025-12-09 15:07:13 +0100

    feat: add script and dataset for tissue types

---
+++ erdera/ontologies/get_tissue_types.py
@@ -0,0 +1,69 @@
+"""Retreive tissue types
+
+This script retrieves tissue types from GTex portal and converts
+it into EMX2 ontology format
+"""
+
+import requests
+import pandas as pd
+from urllib import parse
+
+
+def get_gtex_tissue_types():
+    """Retreive tissue type entries metadata"""
+    url = 'https://gtexportal.org/api/v2/dataset/tissueSiteDetail?datasetId=gtex_v10&page=0&itemsPerPage=250'
+
+    gtex = requests.Session()
+    response = gtex.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_ebi_term_meta(client, code: str = None):
+    """Retrieve ontology term metadata"""
+    encoded_iri = 'http%253A%252F%252Fwww.ebi.ac.uk%252Fefo%252F' \
+        if 'EFO:' in code \
+        else 'http%253A%252F%252Fpurl.obolibrary.org%252Fobo%252F'
+    url = ''.join([
+        'https://www.ebi.ac.uk/ols4/api/ontologies/',
+        code.split(':')[0].lower(),
+        '/terms/',
+        encoded_iri,
+        code.replace(':', '_')
+    ])
+    response = client.get(url)
+    response.raise_for_status()
+
+    response_data = response.json()
+    return {
+        'name': response_data.get('label'),
+        'codesystem': response_data.get('ontology_prefix'),
+        'code': response_data.get('obo_id'),
+        'ontologyTermURI': response_data.get('ontology_iri'),
+        'definition': ' '.join(response_data.get('description'))
+    }
+
+
+if __name__ == '__main__':
+
+    # retrieve tissue types
+    data = pd.DataFrame(get_gtex_tissue_types()['data'])
+    tissues_df = data[[
+        'tissueSiteDetailId',
+        'datasetId',
+        'ontologyId',
+        'ontologyIri'
+    ]]
+
+    # retrieve ontology metadata from each tissue
+    ebi = requests.Session()
+    ontology = []
+    for value in tissues_df['ontologyId'].to_list():
+        term_entry = get_ebi_term_meta(ebi, value)
+        ontology.append(term_entry)
+
+    ontology_df = pd.DataFrame(ontology)
+    ontology_df['order'] = ontology_df.index
+    ontology_df = ontology_df[
+        ['order', 'name', 'codesystem', 'code', 'ontologyTermURI', 'definition']]
+    ontology_df.to_csv('../../model/lookups/tissue types.csv', index=False)
