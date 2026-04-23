aaf4ecfe707de90cdf17886dd47f3757a0cc17d1
dcruvolo <davidruvolo51@gmail.com>
2026-04-23 12:53:54 +0200

    added missing type

--- tests/config.ts
+++ tests/config.ts
@@ -1,3 +1,3 @@
 export const host: string = "http://localhost:8080";
 export const schema: string = "test";
-export const graphqlApi = `${host}/${schema}/graphql`;
+export const graphqlApi: string = `${host}/${schema}/graphql`;
